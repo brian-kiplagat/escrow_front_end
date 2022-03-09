b6cde120f6054c2dd4373dac00191d2cd34820a4
Abdi Adan <abdi.adan@healthcloud.co.ke>
2022-03-09 17:28:16 +0300

    fix: firebase prod environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,16 @@
 
 export const environment = {
   production: false,
-   firebase: {
-  apiKey: "AIzaSyDMsPInM58uPksNu3E9iQ5EhnWBz67CPeQ",
-  authDomain: "scanpal-f74da.firebaseapp.com",
-  databaseURL: "https://scanpal-f74da.firebaseio.com",
-  projectId: "scanpal-f74da",
-  storageBucket: "scanpal-f74da.appspot.com",
-  messagingSenderId: "702443834173",
-  appId: "1:702443834173:web:eea09b202c6abcb5da1354",
-  measurementId: "G-9MVD7S5CEC"
-}
+  firebase: {
+    apiKey: "AIzaSyDMsPInM58uPksNu3E9iQ5EhnWBz67CPeQ",
+    authDomain: "scanpal-f74da.firebaseapp.com",
+    databaseURL: "https://scanpal-f74da.firebaseio.com",
+    projectId: "scanpal-f74da",
+    storageBucket: "scanpal-f74da.appspot.com",
+    messagingSenderId: "702443834173",
+    appId: "1:702443834173:web:eea09b202c6abcb5da1354",
+    measurementId: "G-9MVD7S5CEC"
+  }
 };
 
 /*
