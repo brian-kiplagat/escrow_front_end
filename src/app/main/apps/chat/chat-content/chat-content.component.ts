bbb15b3840e228d9102955f7adef27b11f349e68
wansom <ochiengwarren10@gmail.com>
2022-04-18 06:38:49 +0300

    add chat UI

--- src/app/main/apps/chat/chat-content/chat-content.component.ts
+++ src/app/main/apps/chat/chat-content/chat-content.component.ts
@@ -15,11 +15,26 @@ export class ChatContentComponent implements OnInit {
 
     // Public
     public activeChat: Boolean;
-    public chats;
-    public chatUser;
+    public chats =[
+        {
+            senderId:1,
+            message:"hello"
+        },
+        {
+            senderId:2,
+            message:"hello there"
+        }
+    ];
+    public chatUser={
+        fullName:"ochieng Warren",
+        userId:1,
+        avatar:"assets/images/avatars/12-small.png",
+        status:"online"
+    };
     public userProfile;
     public chatMessage = '';
     public newChat;
+    public startConvo:Boolean = true
 
     /**
      * Constructor
@@ -46,25 +61,25 @@ export class ChatContentComponent implements OnInit {
         console.log(this.chatMessage);
         console.log(this.newChat)
        // If chat data is available (update chat)
-        if (this.chats.chat) {
-            if (this.newChat.message !== '') {
-                this.chats.chat.push(this.newChat);
-                this._chatService.updateChat(this.chats);
-                this.chatMessage = '';//Reset the input to an empty value
-                this.chats.chat.push({
-                    message: 'lol that is kind of sus',
-                    time: 'Mon Dec 10 2018 07:46:43 GMT+0000 (GMT)',
-                    senderId: 1
-                });//Send a sample bot reply..might use this to show replies/incoming message
-                setTimeout(() => {
-                    this.scrolltop = this.scrollMe?.nativeElement.scrollHeight;
-                }, 0);
-            }
-        }
+        // if (this.chats.chat) {
+        //     if (this.newChat.message !== '') {
+        //         this.chats.chat.push(this.newChat);
+        //         this._chatService.updateChat(this.chats);
+        //         this.chatMessage = '';//Reset the input to an empty value
+        //         this.chats.chat.push({
+        //             message: 'lol that is kind of sus',
+        //             time: 'Mon Dec 10 2018 07:46:43 GMT+0000 (GMT)',
+        //             senderId: 1
+        //         });//Send a sample bot reply..might use this to show replies/incoming message
+        //         setTimeout(() => {
+        //             this.scrolltop = this.scrollMe?.nativeElement.scrollHeight;
+        //         }, 0);
+        //     }
+       // }
         // Else create new chat
-        else {
-            this._chatService.createNewChat(this.chatUser.id, this.newChat);
-        }
+        // else {
+        //    // this._chatService.createNewChat(this.chatUser.id, this.newChat);
+        // }
     }
 
     /**
@@ -83,25 +98,26 @@ export class ChatContentComponent implements OnInit {
      * On init
      */
     ngOnInit(): void {
+        this.activeChat = false
         // Subscribe to Chat Change
-        this._chatService.onChatOpenChange.subscribe(res => {
-            this.chatMessage = '';
-            this.activeChat = res;
-            setTimeout(() => {
-                this.scrolltop = this.scrollMe?.nativeElement.scrollHeight;
-            }, 0);
-        });
+        // this._chatService.onChatOpenChange.subscribe(res => {
+        //     this.chatMessage = '';
+        //     this.activeChat = res;
+        //     setTimeout(() => {
+        //         this.scrolltop = this.scrollMe?.nativeElement.scrollHeight;
+        //     }, 0);
+        // });
 
         // Subscribe to Selected Chat Change
-        this._chatService.onSelectedChatChange.subscribe(res => {
-            this.chats = res;
-        });
+        // this._chatService.onSelectedChatChange.subscribe(res => {
+        //     this.chats = res;
+        // });
 
         // Subscribe to Selected Chat User Change
-        this._chatService.onSelectedChatUserChange.subscribe(res => {
-            this.chatUser = res;
-        });
+        // this._chatService.onSelectedChatUserChange.subscribe(res => {
+        //     this.chatUser = res;
+        // });
 
-        this.userProfile = this._chatService.userProfile;
+       // this.userProfile = this._chatService.userProfile;
     }
 }
