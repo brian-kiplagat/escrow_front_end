44d6dee6f4416fba74aacb7a445790687496c250
Brian Kiplagat <52778622+brian-kiplagat@users.noreply.github.com>
2022-06-24 05:09:49 +0300

    Added timer

--- src/app/main/apps/chat/chat-sidebars/chat-sidebar/chat-sidebar.component.ts
+++ src/app/main/apps/chat/chat-sidebars/chat-sidebar/chat-sidebar.component.ts
@@ -2,9 +2,10 @@ import {Component, OnInit, Input} from '@angular/core';
 import {first} from 'rxjs/operators';
 import {CoreSidebarService} from '@core/components/core-sidebar/core-sidebar.service';
 import {ChatService} from 'app/main/apps/chat/chat.service';
-import { ToastrService, GlobalConfig } from 'ngx-toastr';
+import {ToastrService, GlobalConfig} from 'ngx-toastr';
 import Swal from "sweetalert2";
 import {Router} from "@angular/router";
+import {interval, Subscription, timer} from "rxjs";
 
 @Component({
   selector: 'app-chat-sidebar',
@@ -21,9 +22,17 @@ export class ChatSidebarComponent implements OnInit {
   public selectedIndex = null;
   public userProfile;
   private options: GlobalConfig;
-  public user:any ={}
+  public user: any = {}
   public storage: any;
 
+  //TIMER
+  countDown: Subscription;
+  counter;//1800
+  tick = 1000;
+  public minutes;
+  public seconds
+  //TIMER
+  public mmss: string;
 
 
   /**
@@ -35,6 +44,7 @@ export class ChatSidebarComponent implements OnInit {
   constructor(private _chatService: ChatService, private _coreSidebarService: CoreSidebarService, private toastr: ToastrService) {
     this.options = this.toastr.toastrConfig;
   }
+
   // Public Methods
   // -----------------------------------------------------------------------------------------------------
   /**
@@ -53,6 +63,7 @@ export class ChatSidebarComponent implements OnInit {
       }
     });
   }
+
   /**
    * Toggle Sidebar
    *
@@ -61,6 +72,7 @@ export class ChatSidebarComponent implements OnInit {
   toggleSidebar(name) {
     this._coreSidebarService.getSidebarRegistry(name).toggleOpen();
   }
+
   /**
    * Set Index
    *
@@ -69,14 +81,31 @@ export class ChatSidebarComponent implements OnInit {
   setIndex(index: number) {
     this.selectedIndex = index;
   }
+
   // Lifecycle Hooks
   // -----------------------------------------------------------------------------------------------------
   buyer_online: boolean = true;
+
   /**
    * On init
    */
   ngOnInit(): void {
     // Subscribe to contacts
+    const secs_since_start = new Date('2022-06-24 05:00:27').getTime() / 1000;
+    //console.log(secs_since_start)
+    const current_time_stamp = Math.floor(Date.now() / 1000)
+    let dif =1800 -( current_time_stamp - secs_since_start);
+
+    this.counter = dif
+    if (dif > 1800) {
+    console.log(dif)
+    }
+    this.countDown = timer(0, this.tick)
+      .subscribe(() => {
+        --this.counter
+        this.mmss = this.transform(this.counter)
+
+      })
     this.storage = JSON.parse(localStorage.getItem('user'));
     this._chatService.onContactsChange.subscribe(res => {
       this.contacts = res;
@@ -119,8 +148,16 @@ export class ChatSidebarComponent implements OnInit {
     this.openChat(1);//1
 
   }
+
+  transform(value: number): string {
+    //MM:SS format
+    const minutes: number = Math.floor(value / 60);
+    return ('00' + minutes).slice(-2) + ':' + ('00' + Math.floor(value - minutes * 60)).slice(-2);
+
+  }
+
   mark_paid(id: any) {
-    Swal.fire ({
+    Swal.fire({
       title: 'Are you sure?',
       text: "You must ensure that you have sent the money first before clicking this button. Providing false information or coinlocking will cause your account to be banned",
       icon: 'warning',
@@ -134,23 +171,23 @@ export class ChatSidebarComponent implements OnInit {
       }
     }).then(function (result) {
       if (result.value) {
-        let user =  JSON.parse(localStorage.getItem('user'))
+        let user = JSON.parse(localStorage.getItem('user'))
         const headerDict = {
           'Content-Type': 'application/json',
           Accept: 'application/json',
-          token:user.token,
+          token: user.token,
           username: user.username
         }
         const requestOptions = {
           headers: new Headers(headerDict),
         };
-        return fetch('https://api.coinlif.com/api/coin/v1/markPaid/'+id,requestOptions)
+        return fetch('https://api.coinlif.com/api/coin/v1/markPaid/' + id, requestOptions)
           .then(function (response) {
             console.log(response);
             if (!response.ok) {
 
               throw new Error(response.statusText);
-            }else{
+            } else {
 
               Swal.fire({
                 title: 'Trade Marked as Paid',
@@ -160,7 +197,7 @@ export class ChatSidebarComponent implements OnInit {
                   confirmButton: 'btn btn-success'
                 }
               });
-             location.reload();
+              location.reload();
             }
             return response.json();
           })
@@ -178,8 +215,9 @@ export class ChatSidebarComponent implements OnInit {
       }
     });
   }
+
   cancel_trade(id) {
-    Swal.fire ({
+    Swal.fire({
       title: 'Are you sure?',
       text: "If you cancel the trade, well return the escrow amount back to the seller. Otherwise if you had made a payment and have any problem, click back and start a dispute.",
       icon: 'warning',
@@ -194,24 +232,24 @@ export class ChatSidebarComponent implements OnInit {
       }
     }).then(function (result) {
       if (result.value) {
-        let user =  JSON.parse(localStorage.getItem('user'))
+        let user = JSON.parse(localStorage.getItem('user'))
         const headerDict = {
           'Content-Type': 'application/json',
           Accept: 'application/json',
           token: user.token,
-          username:  user.username
+          username: user.username
         }
 
         const requestOptions = {
           headers: new Headers(headerDict),
         };
-        return fetch('https://api.coinlif.com/api/coin/v1/cancelTrade/'+id,requestOptions)
+        return fetch('https://api.coinlif.com/api/coin/v1/cancelTrade/' + id, requestOptions)
           .then(function (response) {
             console.log(response);
             if (!response.ok) {
 
               throw new Error(response.statusText);
-            }else{
+            } else {
 
               Swal.fire({
                 title: 'Trade Cancelled',
@@ -239,6 +277,7 @@ export class ChatSidebarComponent implements OnInit {
       }
     });
   }
+
   open_dispute(id) {
     Swal.mixin({
       input: 'text',
@@ -271,7 +310,7 @@ export class ChatSidebarComponent implements OnInit {
             title: 'DISPUTE OPEN',
             html: 'Provide the moderator with as much information and evidence as you can. Check your email for details on how to win. Well be joining you shortly to help you',
             confirmButtonText: 'OKAY',
-            customClass: { confirmButton: 'btn btn-primary' }
+            customClass: {confirmButton: 'btn btn-primary'}
           })
 
         }
@@ -280,7 +319,7 @@ export class ChatSidebarComponent implements OnInit {
   }
 
   view_offer(idd: any) {
-    window.location.href = '/offers/bitcoin/details/'+idd
+    window.location.href = '/offers/bitcoin/details/' + idd
 
   }
 }
