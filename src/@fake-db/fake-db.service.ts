c3600ebf6aec933fc765177f33c3e5aef104815f
Brian Kiplagat <52778622+brian-kiplagat@users.noreply.github.com>
2022-07-20 02:32:48 +0300

    Clean

--- src/@fake-db/fake-db.service.ts
+++ src/@fake-db/fake-db.service.ts
@@ -2,30 +2,22 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 import { accountSettingsFakeData } from '@fake-db/account-settings.data';
 import { BlogFakeData } from '@fake-db/blog.data';
-import { CalendarFakeData } from '@fake-db/calendar.data';
 import { CardAnalyticsData } from '@fake-db/card-analytics.data';
 import { CardStatisticsData } from '@fake-db/card-statistics.data';
 import { ChatWidgetFakeData } from '@fake-db/chat-widget.data';
-import { ChatFakeData } from '@fake-db/chat.data';
 import { DashboardFakeData } from '@fake-db/dashboard.data';
-import { DatatableFakeData } from '@fake-db/datatables';
 import { EcommerceFakeData } from '@fake-db/ecommerce.data';
-import { EmailFakeData } from '@fake-db/email.data';
 import { FAQFakeData } from '@fake-db/faq.data';
-import { InvoiceFakeData } from '@fake-db/invoice.data';
 import { KBFakeData } from '@fake-db/knowledge-base.data';
-import { NotificationsFakeData } from '@fake-db/notifications.data';
 import { PricingFakeData } from '@fake-db/pricing.data';
 import { ProfileFakeData } from '@fake-db/profile.data';
 import { SearchFakeData } from '@fake-db/search.data';
 import { TodoFakeData } from '@fake-db/todo.data';
-import { UsersFakeData } from '@fake-db/users.data';
 
 export class FakeDbService implements InMemoryDbService {
   createDb(): any {
     return {
-      // Data-table
-      'datatable-rows': DatatableFakeData.rows,
+
 
       // Account Settings
       'account-settings-data': accountSettingsFakeData.data,
@@ -51,11 +43,6 @@ export class FakeDbService implements InMemoryDbService {
       // Card Analytics
       'card-analytics-data': CardAnalyticsData.data,
 
-      // Users
-      'users-data': UsersFakeData.users,
-
-      // Invoice
-      'invoice-data': InvoiceFakeData.invoices,
 
       // Todo
       'todos-data': TodoFakeData.tasks,
@@ -63,34 +50,19 @@ export class FakeDbService implements InMemoryDbService {
       'todos-filters': TodoFakeData.filters,
       'todos-tags': TodoFakeData.tags,
 
-      // Email
-      'emails-data': EmailFakeData.emails,
-      'email-folders': EmailFakeData.folders,
-      'email-labels': EmailFakeData.labels,
-
       // E-Commerce
       'ecommerce-products': EcommerceFakeData.products,
       'ecommerce-relatedProducts': EcommerceFakeData.relatedProducts,
       'ecommerce-userWishlist': EcommerceFakeData.userWishlist,
       'ecommerce-userCart': EcommerceFakeData.userCart,
 
-      // Chat
-      'chat-profileUser': ChatFakeData.profileUser,
-      'chat-contacts': ChatFakeData.contacts,
-      'chat-chats': ChatFakeData.chats,
 
       // Chat Widget
       'chat-widget-data': ChatWidgetFakeData.data,
 
-      // Calendar
-      'calendar-events': CalendarFakeData.events,
-      'calendar-filter': CalendarFakeData.calendar,
-
       // Search
       'search-data': SearchFakeData.search,
 
-      // Notifications
-      'notifications-data': NotificationsFakeData.data,
 
       // Dashboard
       'dashboard-data': DashboardFakeData.data
