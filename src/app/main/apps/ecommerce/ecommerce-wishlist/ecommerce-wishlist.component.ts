4e7ade7f8fee47118e291bf98a5a24d25b4684a2
USER <kiplagatbrian18@gmail.com>
2023-04-05 21:36:41 +0300

    Add multiselect

--- src/app/main/apps/ecommerce/ecommerce-wishlist/ecommerce-wishlist.component.ts
+++ src/app/main/apps/ecommerce/ecommerce-wishlist/ecommerce-wishlist.component.ts
@@ -1,116 +1,119 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
-import { CoreSidebarService } from '@core/components/core-sidebar/core-sidebar.service';
+import {CoreSidebarService} from '@core/components/core-sidebar/core-sidebar.service';
 
-import { FirebaseService } from '../../../../services/firebase.service';
+import {FirebaseService} from '../../../../services/firebase.service';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+
 @Component({
-    selector: 'app-ecommerce-wishlist',
-    templateUrl: './ecommerce-wishlist.component.html',
-    styleUrls: ['./ecommerce-wishlist.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    host: { class: 'ecommerce-application' }
+  selector: 'app-ecommerce-wishlist',
+  templateUrl: './ecommerce-wishlist.component.html',
+  styleUrls: ['./ecommerce-wishlist.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: {class: 'ecommerce-application'}
 })
 export class EcommerceWishlistComponent implements OnInit {
-    // Public
+  // Public
 
-    public contentHeader: object;
-    public shopSidebarToggle = false;
-    public shopSidebarReset = false;
-    public gridViewRef = true;
-    public products;
-    public wishlist;
-    public cartList;
-    public page = 1;
-    public pageSize = 9;
-    public searchText = '';
-    public selectBasic: any[] = ['Bank Transfer', 'Mpesa', 'Paypal', 'Skrill'];
-    public selectBasicLoading = false;
-    public offers = []
-    public currency:any[] = []
-    public currencies =[]
-    public methods =[]
-    public   amount =0
-    public type:string ="buy"
-    public filters ={
-         currency:"",
-         method:"",
+  public contentHeader: object;
+  public shopSidebarToggle = false;
+  public shopSidebarReset = false;
+  public gridViewRef = true;
+  public products;
+  public wishlist;
+  public cartList;
+  public page = 1;
+  public pageSize = 9;
+  public searchText = '';
+  public selectBasic: any[] = ['Bank Transfer', 'Mpesa', 'Paypal', 'Skrill'];
+  public selectBasicLoading = false;
+  public offers = []
+  public currency: any[] = []
+  public currencies = []
+  public methods = []
+  public amount = 0
+  public type: string = "buy"
+  public filters = {
+    currency: "",
+    method: "",
 
-    }
-
-
-
-    /**
-     *
-     * @param {CoreSidebarService} _coreSidebarService
-     * @param {FirebaseService}_fb
-     */
-    constructor(
-        private _coreSidebarService: CoreSidebarService,
-        private _fb: FirebaseService
-    ) { }
-
-    // Public Methods
-    // -----------------------------------------------------------------------------------------------------
-
-    /**
-     * Toggle Sidebar
-     *
-     * @param name
-     */
-    toggleSidebar(name): void {
-        this._coreSidebarService.getSidebarRegistry(name).toggleOpen();
-    }
-
-    /**
-     * Update to List View
-     */
-    listView() {
-        this.gridViewRef = false;
-    }
-
-    /**
-     * Update to Grid View
-     */
-    gridView() {
-        this.gridViewRef = true;
-    }
-
-    /**
-     * Sort Product
-     */
-
-    /**
-     * On init
-     */
-    ngOnInit(): void {
-        this._fb
-            .getOffers("buy").subscribe((data:any) => {
-                // this.offers = data['data']['payload']
-                this.offers = data.responseMessage
-                //console.log(data)
-            })
-
-            this._fb.getCurrency().subscribe((data:any)=>{
-                this.currencies =data.responseMessage.currencies
-                this.methods = data.responseMessage.methods
-                  //console.log( this.methods)
-              },(error)=>{
-                console.log(error)
-              })
+  }
 
 
+  /**
+   *
+   * @param {CoreSidebarService} _coreSidebarService
+   * @param {FirebaseService}_fb
+   * @param modalService
+   */
+  constructor(
+    private _coreSidebarService: CoreSidebarService,
+    private _fb: FirebaseService, private modalService: NgbModal
+  ) {
+  }
+
+  // Public Methods
+  // -----------------------------------------------------------------------------------------------------
+
+  /**
+   * Toggle Sidebar
+   *
+   * @param name
+   */
+  toggleSidebar(name): void {
+    this._coreSidebarService.getSidebarRegistry(name).toggleOpen();
+  }
+
+  /**
+   * Update to List View
+   */
+  listView() {
+    this.gridViewRef = false;
+  }
+
+  /**
+   * Update to Grid View
+   */
+  gridView() {
+    this.gridViewRef = true;
+  }
+
+  /**
+   * Sort Product
+   */
+
+  /**
+   * On init
+   */
+  ngOnInit(): void {
+    this._fb
+      .getOffers("buy").subscribe((data: any) => {
+      // this.offers = data['data']['payload']
+      this.offers = data.responseMessage
+      //console.log(data)
+    })
+
+    this._fb.getCurrency().subscribe((data: any) => {
+      this.currencies = data.responseMessage.currencies
+      this.methods = data.responseMessage.methods
+      //console.log( this.methods)
+    }, (error) => {
+      console.log(error)
+    })
 
 
-    }
-    onNotify(value:number){
-         let user =JSON.parse(localStorage.getItem('user'))
+  }
+
+  onNotify(value: number) {
+    let user = JSON.parse(localStorage.getItem('user'))
 
 
-    }
-    onSliderChange(value:any){
-        let user =JSON.parse(localStorage.getItem('user'))
+  }
 
-    }
+  onSliderChange(value: any) {
+    let user = JSON.parse(localStorage.getItem('user'))
+
+  }
 
   //filter by tag
   onTagChange(value: string) {
@@ -123,32 +126,54 @@ export class EcommerceWishlistComponent implements OnInit {
     return arr.split(',');
 
   }
-        //filter offers
-        filterOffers(){
-            let user =JSON.parse(localStorage.getItem('user'))
-            this._fb
-                .getOffers("sell").subscribe((data:any)=>{
-                    this.offers = data.responseMessage
 
-                    this.offers=data.responseMessage.filter((item:any) =>{
+  //filter offers
+  filterOffers() {
+    let user = JSON.parse(localStorage.getItem('user'))
+    this._fb
+      .getOffers("sell").subscribe((data: any) => {
+      this.offers = data.responseMessage
 
-                        if(this.amount!=0&&this.amount < Number(item.minimum)|| this.amount > Number(item.maximum)){
+      this.offers = data.responseMessage.filter((item: any) => {
 
-                            return false
-                        }
-                        if(this.filters.currency&& item.currency != this.filters.currency){
-                            return false
-                        }
-                        if(this.filters.method&& item.method != this.filters.method){
-                            return false
-                        }
-                        return true;
-                      });
+        if (this.amount != 0 && this.amount < Number(item.minimum) || this.amount > Number(item.maximum)) {
 
-                      console.log(this.offers)
-                })
-
+          return false
         }
+        if (this.filters.currency && item.currency != this.filters.currency) {
+          return false
+        }
+        if (this.filters.method && item.method != this.filters.method) {
+          return false
+        }
+        return true;
+      });
+
+      console.log(this.offers)
+    })
+
+  }
+
+
+// ng-select in model
+  modalSelectOpen(modalSelect) {
+    this.modalService.open(modalSelect, {
+      windowClass: 'modal'
+    });
+  }
+
+
+  changeFn(val : string) {
+    if (val == null) {
+      document.querySelector('#btn-text').innerHTML = 'USD';
+    } else {
+      document.querySelector('#btn-text').innerHTML = val;
+      this.modalService.dismissAll();
+    }
+    //console.log("Dropdown selection:", val);
+
+
+  }
 
 
 }
